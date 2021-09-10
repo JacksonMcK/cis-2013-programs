fe75056b107ace8824ac139e74416184cf62b26c
JacksonMcK <jam7735@utulsa.edu>
2021-09-09 20:24:41 -0500

    Age HO#3 CH#2
    Age JS and HTML

---
+++ Age.js
@@ -0,0 +1,8 @@
+var floatAgeYears, floatAgeDays, floatAgeMonths, floatAgeWeeks, floatAgeFortnights
+floatAgeYears= parseFloat(prompt("Please Enter Your Age in Years!"));
+floatAgeDays= parseFloat(floatAgeYears*365.25);
+floatAgeWeeks= parseFloat(floatAgeDays/7);
+floatAgeFortnights= parseFloat(floatAgeDays/14);
+floatAgeMonths= parseFloat(floatAgeYears*12);
+alert("Your age in Days is " floatAgeDays, "Your age in Weeks is " floatAgeWeeks,
+      "Your age in Fortnights is " floatAgeFortnights, "Your age in Months is " floatAgeMonths);
