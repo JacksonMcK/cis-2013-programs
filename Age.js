96e037a71d1758befab0fc74caf5d75eeb58b2a8
JacksonMcK <jam7735@utulsa.edu>
2021-09-09 23:25:43 -0500

    Update Age.js

--- Age.js
+++ Age.js
@@ -1,8 +1,7 @@
-var floatAgeYears, floatAgeDays, floatAgeMonths, floatAgeWeeks, floatAgeFortnights
+var floatAgeYears, floatAgeDays, floatAgeMonths, floatAgeWeeks, floatAgeFortnights;
      floatAgeYears= prompt("Please Enter Your Age in Years!");
      floatAgeDays= floatAgeYears*365.25;
      floatAgeWeeks= floatAgeDays/7;
      floatAgeFortnights= floatAgeDays/14;
      floatAgeMonths= floatAgeYears*12;
-     alert("Your age in Days is " floatAgeDays, "Your age in Weeks is " floatAgeWeeks,
-           "Your age in Fortnights is " floatAgeFortnights, "Your age in Months is " floatAgeMonths);
+     alert("Meow");
